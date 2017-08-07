b2d351c5af510341ea0cbfa3b1eb099e6bb40c07
beakyangsu <qordidtn02@gmail.com>
2017-08-07 22:55:22 +0900

    Update tensorflow_basic.py

--- tensorflow_tutorial/tensorflow_basic.py
+++ tensorflow_tutorial/tensorflow_basic.py
@@ -33,7 +33,7 @@ for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
 #minimize difference between analized image and trainning image
-#done tranning
+#done traning
 
 correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
